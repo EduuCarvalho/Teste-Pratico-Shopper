dd388a9d8218037f19597e5351cc59f050af7779
EduuCarvalho <educarvalho.engcivil@gmail.com>
2023-05-14 23:50:03 -0300

    feat: error 422 if new pric is lower than cost pri

--- Shopper-api/src/service/price-service.ts
+++ Shopper-api/src/service/price-service.ts
@@ -11,6 +11,10 @@ export async function updateProductService (product:Product) {
         if (productDB.length === 0) {
             throw createError(404,"Produto não encontrado!")
           }
+    
+        if(parseFloat(productDB[0].cost_price)> product.new_price){
+            throw createError(422,"Novo preço deve ser maior que o preço de custo!")
+        }  
     }
     catch (err) {
         throw err
